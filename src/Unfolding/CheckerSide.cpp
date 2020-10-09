57f1c17f972228dcc65f34b842cb20f56feee4ca
Ehsan Azimi <eazimi@ehsan.irisa.fr>
2020-10-09 16:31:07 +0200

    Not required Actor instance removed

--- src/Unfolding/CheckerSide.cpp
+++ src/Unfolding/CheckerSide.cpp
@@ -4,8 +4,6 @@ namespace uc
 {
     void CheckerSide::run(std::shared_ptr<AppSide> app_side)
     {
-        auto actors = app_side->get_actors();
-        auto mbs = app_side->get_mbs();
         uc_->explore(app_side);
     }
 } // namespace uc
